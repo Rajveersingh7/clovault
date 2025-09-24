e14f5959cf3f28dbf51dbc8b0181f777ba426752
Rajveersingh7 <145246113+Rajveersingh7@users.noreply.github.com>
2025-09-24 22:43:42 +0530

    made more explicit

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import GitHub from "next-auth/providers/github";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
   providers: [GitHub],
+  trustHost: true,
   callbacks: {
     async jwt({token, account}) {
       if (account?.providerAccountId) {
